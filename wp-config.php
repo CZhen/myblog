460a4a695c1572772ec1d6946ac8819329fc2446
CZhen <632548328@qq.com>
2015-10-12 18:05:05 +0800

    edit db config

--- wp-config.php
+++ wp-config.php
@@ -15,13 +15,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wp_myblog');
+define('DB_NAME', 'cz_blog');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', 'cz');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cz123456');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
